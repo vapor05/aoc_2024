564933843cf25914be64eee40eea7a7f2b232ddd
vapor05 <nicholasbocchini@gmail.com>
2024-12-09 22:19:35 -0600

    solutions for problems 4 and 5

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import typing
+import importlib
 import argparse
-import problems
 
-solve_funcs: dict[str, dict[str, typing.Callable[[], str]]] = {
-    "1": {"1": problems.problem1_part1, "2": problems.problem1_part2},
-    "2": {"1": problems.problem2_part1, "2": problems.problem2_part2},
-    "3": {"1": problems.problem3_part1, "2": problems.problem3_part2},
-}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -15,19 +9,18 @@ if __name__ == "__main__":
     )
     parser.add_argument("problem")
     parser.add_argument("--part", default="1", required=False)
+    parser.add_argument("--test", action="store_true", required=False)
     args = parser.parse_args()
     print(f"Running problem {args.problem} part {args.part}...")
-    solve_fns = solve_funcs.get(args.problem)
+    problems = importlib.import_module("problems")
 
-    if solve_fns is None:
-        print(f"No solution function defined for problem {args.problem}")
-    else:
-        solve_fn = solve_fns.get(args.part)
+    try:
+        solve_fn = getattr(problems, f"problem{args.problem}_part{args.part}")
+        solution = solve_fn(test=args.test)
+        print(f"Solution to problem {args.problem} part {args.part} is {solution}")
+    except AttributeError as ex:
+        print(
+            f"No solution function defined for part {args.part} of problem {args.problem}"
+        )
 
-        if solve_fn is None:
-            print(
-                f"No solution function defined for part {args.part} of problem {args.problem}"
-            )
-        else:
-            solution = solve_fn()
-            print(f"Solution to problem {args.problem} part {args.part} is {solution}")
+    
--- problems.py
+++ problems.py
@@ -1,3 +1,4 @@
+import functools
 import re
 import pathlib
 
@@ -22,7 +23,7 @@ def _load_problem_one_data() -> tuple[list[int], list[int]]:
     return ll, rl
 
 
-def problem1_part1() -> str:
+def problem1_part1(test: bool = False) -> str:
     ll, rl = _load_problem_one_data()
 
     ll.sort()
@@ -32,7 +33,7 @@ def problem1_part1() -> str:
     return str(sum(dists))
 
 
-def problem1_part2() -> str:
+def problem1_part2(test: bool = False) -> str:
     ll, rl = _load_problem_one_data()
     mult_map: dict[int, int] = {}
 
@@ -58,7 +59,7 @@ def _load_problem_2_data() -> list[list[int]]:
     return reports
 
 
-def problem2_part1() -> str:
+def problem2_part1(test: bool = False) -> str:
     reports: list[list[int]] = _load_problem_2_data()
     safe = []
 
@@ -92,7 +93,7 @@ def problem2_part1() -> str:
     return str(len(safe))
 
 
-def problem2_part2() -> str:
+def problem2_part2(test: bool = False) -> str:
     reports: list[list[int]] = _load_problem_2_data()
     safe = []
 
@@ -143,7 +144,7 @@ def _load_problem_3_data() -> str:
     return data
 
 
-def problem3_part1() -> str:
+def problem3_part1(test: bool = False) -> str:
     instructions = _load_problem_3_data()
     mul_pattern = re.compile(r"(mul\(\d{1,3},\d{1,3}\))")
     matches = mul_pattern.findall(instructions)
@@ -153,7 +154,7 @@ def problem3_part1() -> str:
     return str(sum(products))
 
 
-def problem3_part2() -> str:
+def problem3_part2(test: bool = False) -> str:
     memory = _load_problem_3_data()
     instruction_pattern = re.compile(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don\'t\(\))")
     matches = instruction_pattern.findall(memory)
@@ -192,13 +193,268 @@ def problem3_part2() -> str:
     return str(sum)
 
 
-def problem4_part1() -> str:
-    pass
-    # check each cell for
-    # if X
-    #  then check all 8 neighbors for next letter
-    #   if neighbor match next letter:
-    #    check its 8 neighbors for next letter
-    #     keep going until find all or find end
-    #   if no matches go to next cell
-    # if not go to next cell
+class Letter:
+
+    def __init__(self, char: str, row: int, col: int):
+        self.char = char
+        self.row = row
+        self.col = col
+
+    def __repr__(self):
+        return f"{self.char} < row: {self.row}, col: {self.col} >"
+
+
+def _load_problem4_data(test: bool = False) -> list[list[Letter]]:
+    name = "problem4.txt" if not test else "test_problem4.txt"
+    fl = _problem_data_file(name)
+    data: list[list[str]] = []
+
+    with open(fl) as f:
+        for row, l in enumerate(f.readlines()):
+            data.append([Letter(c, row, col) for col, c in enumerate(l.strip())])
+
+    return data
+
+
+def problem4_part1(test: bool = False) -> str:
+    match: dict[str, str] = {"M": "A", "A": "S", "S": None}
+    data = _load_problem4_data(test)
+    rows = len(data)
+    finds = 0
+
+    for i, row in enumerate(data):
+        cols = len(row)
+
+        for j, letter in enumerate(row):
+            if letter.char != "X":
+                continue
+
+            check_list: list[Letter] = []
+            has_previous_row = i != 0
+            has_next_row = i + 1 < rows
+            has_previous_col = j != 0
+            has_next_col = j + 1 < cols
+
+            if has_previous_row:
+                previous = data[i - 1]
+
+                if has_previous_col:
+                    check_list.append(previous[j - 1])
+
+                check_list.append(previous[j])
+
+                if has_next_col:
+                    check_list.append(previous[j + 1])
+
+            if has_previous_col:
+                check_list.append(row[j - 1])
+
+            if has_next_col:
+                check_list.append(row[j + 1])
+
+            if has_next_row:
+                next = data[i + 1]
+
+                if has_previous_col:
+                    check_list.append(next[j - 1])
+
+                check_list.append(next[j])
+
+                if has_next_col:
+                    check_list.append(next[j + 1])
+
+            want = "M"
+            letter_want = "M"
+
+            while len(check_list) > 0:
+                check = check_list.pop(0)
+
+                if want == check.char:
+                    want = match.get(check.char)
+
+                    if want is None:
+                        finds += 1
+                        want = letter_want
+                        continue
+
+                    if letter.row < check.row:
+                        if check.row + 1 < rows:
+                            r = data[check.row + 1]
+                        else:
+                            r = None
+                    elif letter.row == check.row:
+                        r = row
+                    else:
+                        if check.row != 0:
+                            r = data[check.row - 1]
+                        else:
+                            r = None
+
+                    if r is not None:
+                        if letter.col < check.col:
+                            if check.col + 1 < cols:
+                                check_list.insert(0, r[check.col + 1])
+                        elif letter.col == check.col:
+                            check_list.insert(0, r[check.col])
+                        else:
+                            if check.col != 0:
+                                check_list.insert(0, r[check.col - 1])
+                else:
+                    want = letter_want
+
+    return str(finds)
+
+
+def problem4_part2(test: bool = False) -> str:
+    data = _load_problem4_data(test)
+    rows = len(data)
+    finds = 0
+
+    for i in range(1, rows - 1):
+        row = data[i]
+        cols = len(row)
+
+        for j in range(1, cols - 1):
+            letter = row[j]
+
+            if letter.char == "A":
+                top_left = data[letter.row - 1][letter.col - 1]
+                top_right = data[letter.row - 1][letter.col + 1]
+                bottom_left = data[letter.row + 1][letter.col - 1]
+                bottom_right = data[letter.row + 1][letter.col + 1]
+
+                if top_left.char == "M":
+                    left_reverse = False
+                elif top_left.char == "S":
+                    left_reverse = True
+                else:
+                    continue
+
+                if top_right.char == "M":
+                    right_reverse = False
+                elif top_right.char == "S":
+                    right_reverse = True
+                else:
+                    continue
+
+                if (
+                    (left_reverse and bottom_right.char == "M")
+                    or (not left_reverse and bottom_right.char == "S")
+                ) and (
+                    (right_reverse and bottom_left.char == "M")
+                    or (not right_reverse and bottom_left.char == "S")
+                ):
+                    finds += 1
+
+    return str(finds)
+
+
+def _load_problem5_data(test: bool = False) -> tuple[dict[int, set[int]], list[list[int]]]:
+    name = "problem5.txt" if not test else "test_problem5.txt"
+    fl = _problem_data_file(name)
+    rules: dict[int, set[int]] = {}
+    updates: list[list[int]] = []
+
+    with open(fl) as f:
+        for line in f.readlines():
+            if "|" in line:
+                nums = line.strip().split("|")
+                key = int(nums[0])
+                value = int(nums[1])
+                
+                if key not in rules:
+                    rules[key] = set()
+
+                rules[key].add(value)
+
+            elif "," in line:
+                updates.append([int(n) for n in line.strip().split(",")])
+
+    return rules, updates
+
+
+def problem5_part1(test: bool = False) -> str:
+    rules, updates = _load_problem5_data(test)
+    valid_updates: list[list[int]] = []
+
+    for update in updates:
+        search = {n: i for i, n in enumerate(update)}
+    
+        for i, n in enumerate(update):
+            if n not in rules:
+                continue
+
+            n_rules = rules[n]
+            fail_rules = False
+
+            for rule in n_rules:
+                if rule not in search:
+                    continue
+
+                check_i = search[rule]
+                
+                if i >= check_i:
+                    fail_rules = True
+                    break
+
+            if fail_rules:
+                break
+
+        if not fail_rules:
+            valid_updates.append(update)
+
+    mid_points = [vu[int(len(vu)/2)] for vu in valid_updates]
+    return str(sum(mid_points))
+
+
+def problem5_part2(test: bool = False) -> str:
+    rules, updates = _load_problem5_data(test)
+    corrected_updates: list[list[int]] = []
+    invalid=[]
+
+    for update in updates:
+        search = {n: i for i, n in enumerate(update)}
+    
+        for i, n in enumerate(update):
+            if n not in rules:
+                continue
+
+            n_rules = rules[n]
+            fail_rules = False
+
+            for rule in n_rules:
+                if rule not in search:
+                    continue
+
+                check_i = search[rule]
+                
+                if i >= check_i:
+                    fail_rules = True
+                    break
+
+            if fail_rules:
+                break
+
+        if not fail_rules:
+            continue
+        
+        invalid.append(update)
+        def _sort(a, b) -> int:
+            a_rules = rules.get(a)
+
+            if a_rules is None:
+                return 0
+            
+            if b in a_rules:
+                return -1
+            else:
+                return 1
+        
+        corrected_updates.append(sorted(update, key=functools.cmp_to_key(_sort)))
+
+    mid_points = [vu[int(len(vu)/2)] for vu in corrected_updates]
+    return str(sum(mid_points))
+
+
+
+
